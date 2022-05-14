8134167ef5c33ced46c1f9457b8ceb4a0c364c91
vlad-sheremet <uladzislau.sharamet@tui.co.uk>
2022-05-14 15:28:19 +0300

    commit to test docker build

--- src/main/java/beer/fun/bet/controller/TeamController.java
+++ src/main/java/beer/fun/bet/controller/TeamController.java
@@ -9,14 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/teams1")
+@RequestMapping(value = "/teams")
 public class TeamController
 {
     @Resource
